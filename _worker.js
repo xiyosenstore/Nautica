f69ea352626a09abfdeed2639e43d7a8c50dd41e
xiyosenstore <ziyosenstore@gmail.com>
2025-10-04 04:23:03 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -15,7 +15,6 @@ const neko = "Y2xhc2g=";
 
 const PORTS = [443, 80];
 const PROTOCOLS = [atob(horse), atob(flash), "ss"];
-const SUB_PAGE_URL = "https://foolvpn.me/nautica";
 const KV_PRX_URL = "https://raw.githubusercontent.com/FoolVPN-ID/Nautica/refs/heads/main/kvProxyList.json";
 const PRX_BANK_URL = "https://raw.githubusercontent.com/FoolVPN-ID/Nautica/refs/heads/main/proxyList.txt";
 const DNS_SERVER_ADDRESS = "8.8.8.8";
